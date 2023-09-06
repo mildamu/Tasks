7cf449a5103c9af73d397bb6bf83b4ea073a6449
mildamu <mildapett@gmail.com>
2023-09-06 07:31:51 +0300

    enter

---
+++ js_3week/enter.js
@@ -0,0 +1,4 @@
+prompt("Please enter your name")
+prompt("Please enter your date of birth")
+console.log("(name entered) logged in!")
+alert("Name: (name entered)" + "\n" + "Date of birth: (date of birth entered)")
